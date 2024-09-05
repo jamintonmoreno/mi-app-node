76d6a07f552bf19643b32dfcd3c7dbf2ba3bed29
Jaminton Moreno <jaminton.morenof@gmail.com>
2024-09-05 16:07:15 -0500

    First commit of this project

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Test básico', function () {
+  it('Verifica la función test', function () {
+    expect(true).to.be.true;
+  });
+});
